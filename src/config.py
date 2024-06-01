bc9465601e46170de5b9e89adacece037318f5d3
theSm1th <68665004+theSm1th@users.noreply.github.com>
2024-06-01 16:24:16 +0000

    replaced env-based config.py with json-based config.py

--- src/config.py
+++ src/config.py
@@ -16,51 +16,26 @@ def loadConfig():
         "api": {}
     }
 
-    data["targetFPS"] = int(os.getenv("targetFPS") or 70)
-    data["refreshTime"] = int(os.getenv("refreshTime") or 180)
-    data["fpsTime"] = int(os.getenv("fpsTime") or 180)
-    data["screenRotation"] = int(os.getenv("screenRotation") or 2)
-    data["screenBlankHours"] = os.getenv("screenBlankHours") or ""
-    data["headless"] = False
-    if os.getenv("headless") == "True":
-        data["headless"] = True
+    with open("config.json", "r") as file:
+        file_json = json.load(file)
+    
+    general_json = file_json["general"]
+    journey_json = file_json["journey"]
+    api_json = file_json["api"]
 
-    data["debug"] = False
-    if os.getenv("debug") == "True":
-        data["debug"] = True
-    else:
-        if os.getenv("debug") and os.getenv("debug").isnumeric():
-            data["debug"] = int(os.getenv("debug"))
-
-    data["dualScreen"] = False
-    if os.getenv("dualScreen") == "True":
-        data["dualScreen"] = True
-    data["firstDepartureBold"] = True
-    if os.getenv("firstDepartureBold") == "False":
-        data["firstDepartureBold"] = False
-    data["hoursPattern"] = re.compile("^((2[0-3]|[0-1]?[0-9])-(2[0-3]|[0-1]?[0-9]))$")
-
-    data["journey"]["departureStation"] = os.getenv("departureStation") or "PAD"
-
-    data["journey"]["destinationStation"] = os.getenv("destinationStation") or ""
-    if data["journey"]["destinationStation"] == "null" or data["journey"]["destinationStation"] == "undefined":
-        data["journey"]["destinationStation"] = ""
-
-    data["journey"]["individualStationDepartureTime"] = False
-    if os.getenv("individualStationDepartureTime") == "True":
-        data["journey"]["individualStationDepartureTime"] = True
-
-    data["journey"]["outOfHoursName"] = os.getenv("outOfHoursName") or "London Paddington"
-    data["journey"]["stationAbbr"] = {"International": "Intl."}
-    data["journey"]['timeOffset'] = os.getenv("timeOffset") or "0"
-    data["journey"]["screen1Platform"] = parsePlatformData(os.getenv("screen1Platform"))
-    data["journey"]["screen2Platform"] = parsePlatformData(os.getenv("screen2Platform"))
-
-    data["api"]["apiKey"] = os.getenv("apiKey") or None
-    data["api"]["operatingHours"] = os.getenv("operatingHours") or ""
-
-    data["showDepartureNumbers"] = False
-    if os.getenv("showDepartureNumbers") == "True":
-        data["showDepartureNumbers"] = True
+    for item in general_json:
+        if item == "hoursPattern":
+            data[item] = re.compile(general_json[item])
+        else:
+            data[item] = general_json[item]
+    
+    for item in journey_json:
+        if item in ["screenOnePlatform", "screenTwoPlatform"]:
+            data["journey"][item] = parsePlatformData(journey_json[item]) 
+        else:
+            data["journey"][item] = journey_json[item]
+    
+    for item in api_json:
+        data["api"][item] = api_json[item]
 
     return data
