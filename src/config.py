efb346a0ecff77ac440a503ca1923db770370d55
theSm1th <68665004+theSm1th@users.noreply.github.com>
2024-06-01 18:33:36 +0000

    it works now i promise

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
 import os
 import re
+import json
 
 # validate platform number
 def parsePlatformData(platform):
-    if platform is None:
+    if platform is None or platform == "":
         return ""
     elif bool(re.match(r'^(?:\d{1,2}[A-D]|[A-D]|\d{1,2})$', platform)):
         return platform
@@ -24,13 +25,25 @@ def loadConfig():
     api_json = file_json["api"]
 
     for item in general_json:
+        if general_json[item] in ["False", "True"]:
+            if general_json[item] == "False":
+                general_json[item] = False
+            else:
+                general_json[item] = True
+
         if item == "hoursPattern":
             data[item] = re.compile(general_json[item])
         else:
             data[item] = general_json[item]
     
     for item in journey_json:
-        if item in ["screenOnePlatform", "screenTwoPlatform"]:
+        if journey_json[item] in ["False", "True"]:
+            if journey_json[item] == "False":
+                journey_json[item] = False
+            else:
+                journey_json[item] = True
+
+        if item in ["screen1Platform", "screen1Platform"]:
             data["journey"][item] = parsePlatformData(journey_json[item]) 
         else:
             data["journey"][item] = journey_json[item]
